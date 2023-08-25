af2ae41a5b810c7d677208b7bf0bf5c8c305de05
Asami Doi <asamidoi@chromium.org>
2023-08-25 14:14:20 +0200

    Initial Commit

---
+++ ai-test-app/ai_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_test_appApp.swift
+//  ai-test-app
+//
+//  Created by Asami Doi on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ai_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
